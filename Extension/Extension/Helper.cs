1077f57b86aefa5e95327d8622bd2644a90a458d
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-04-02 01:46:17 +0400

    Extension

---
+++ Extension/Extension/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class Helper
+    {
+        public static bool IsOdd(this int num)
+        {
+            return num % 2 == 1;
+        }
+
+        public static bool IsEven(this int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool HasDigit(this string str)
+        {
+            return str.Any(char.IsDigit);
+        }
+        public static bool CheckPassword(this string password)
+        {
+            if (password.Length < 8)
+                return false;
+
+            bool hasUpper = password.Any(char.IsUpper);
+            bool hasLower = password.Any(char.IsLower);
+            bool hasDigit = password.Any(char.IsDigit);
+            return hasUpper && hasLower && hasDigit;
+        }
+       
+        public static string ToCapitalize(this string text)
+        {
+            return Char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+    }
+}
---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 24;
+            Console.WriteLine($"Cut ededdir:{num.IsEven()}");
+            Console.WriteLine($"Tek ededdir:{num.IsOdd ()}");
+
+            string str = "A2ueu";
+            Console.WriteLine($"Daxilinde reqem var:{ str.HasDigit()}");
+
+            string password = "aA12fjsd";
+            Console.WriteLine($"Sifre qaydalara uygundur:{password .CheckPassword ()}");
+
+            string text = "axcvfvgrtg";
+            Console.WriteLine(text .ToCapitalize ());
+        }
+    }
+}
